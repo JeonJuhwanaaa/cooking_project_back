b4866a18d0da85d212c0e471ff5a0f0a636681fa
JeonJuhwanaaa <w7285@naver.com>
2024-08-17 22:52:35 +0900

    fix:user entity and Mybatics

--- src/main/java/com/recipes/recipes/dto/SignupReqDto.java
+++ src/main/java/com/recipes/recipes/dto/SignupReqDto.java
@@ -10,14 +10,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SignupReqDto {
     private String username;
     private String password;
+    private String name;
+    private int genderId;
     private String email;
+    private String phoneNumber;
+    private String address;
+    private String extraAddress;
 
 
     public User toEntity(BCryptPasswordEncoder passwordEncoder){
         return User.builder()
                 .username(username)
                 .password(passwordEncoder.encode(password))
+                .name(name)
+                .genderId(genderId)
                 .email(email)
+                .phoneNumber(phoneNumber)
+                .address(address)
+                .extraAddress(extraAddress)
                 .build();
     }
 
--- src/main/java/com/recipes/recipes/entity/User.java
+++ src/main/java/com/recipes/recipes/entity/User.java
@@ -16,7 +16,12 @@ public class User {
     private int userId;
     private String username;
     private String password;
+    private String name;
+    private int genderId;
     private String email;
+    private String phoneNumber;
+    private String address;
+    private String extraAddress;
     private LocalDateTime createDate;
     private LocalDateTime updateDate;
 
